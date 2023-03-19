aff854d81148d9bb0890142c5bb8c56987c7db62
mellymal-appdevve4ever <128302311+mellymal-appdevve4ever@users.noreply.github.com>
2023-03-19 19:42:46 +0530

    Update streamlit_app.py
    added few more texts and emoji's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('breakfast Menu')
+streamlit.header(' 🥣breakfast Menu')
 streamlit.text('my lunch menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
